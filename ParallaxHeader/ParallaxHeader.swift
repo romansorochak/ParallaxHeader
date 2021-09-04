f1089648f7adaefb144121eed72633e8706efbcf
MussaCharles <mussacharles50@gmail.com>
2021-09-04 16:45:52 +0900

    Fix typo,ignoreToSafeAreaInset -> ignoreTopSafeAreaInset

--- ParallaxHeader/ParallaxHeader.swift
+++ ParallaxHeader/ParallaxHeader.swift
@@ -65,7 +65,7 @@ public class ParallaxHeader: NSObject {
     
     /// This is to support a usecase where a parallax header is being used at the very top edge of the screen.
     /// If this is set to true then the header will be pinned at the top of the scrollView. Default value is `false`
-    public var ignoreToSafeAreaInset:Bool = false
+    public var ignoreTopSafeAreaInset:Bool = false
     
     private weak var _scrollView: UIScrollView?
     var scrollView: UIScrollView! {
@@ -442,7 +442,7 @@ public class ParallaxHeader: NSObject {
         let minimumHeight = min(self.minimumHeight, self.height)
         var relativeYOffset = scrollView.contentOffset.y + scrollView.contentInset.top - height
         if #available(iOS 11.0, *) {
-            if !ignoreToSafeAreaInset {
+            if !ignoreTopSafeAreaInset {
                 relativeYOffset += scrollView.safeAreaInsets.top
             }
         }
